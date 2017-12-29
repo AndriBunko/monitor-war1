494cd74fb76a12ade3dd206280e67558df57935e
AndriBunko <andr1bunk02@gmail.com>
2017-12-29 14:06:51 +0200

    vershion-3

--- src/main/java/com/abunko/config/AppConfig.java
+++ src/main/java/com/abunko/config/AppConfig.java
@@ -91,7 +91,7 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     public UrlBasedViewResolver setupViewResolver() {
         UrlBasedViewResolver resolver = new UrlBasedViewResolver();
         resolver.setPrefix("/WEB-INF/pages/");
-        resolver.setSuffix(".jsp");
+       // resolver.setSuffix(".jsp");
         resolver.setViewClass(JstlView.class);
         resolver.setOrder(1);
         return resolver;
